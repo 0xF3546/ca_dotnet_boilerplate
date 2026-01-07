b04bb0d65f50c613277ae3d60a2bbeca4154ab56
0xF3546 <99101632+0xF3546@users.noreply.github.com>
2026-01-07 07:55:04 +0100

    Add email service with templates and configuration
    Introduced EmailService and IEmailService interfaces for sending emails, supporting templates and attachments. Added EmailTemplate enum for predefined email formats. Updated appsettings.json to include SMTP and email configuration.

---
+++ src/backend.Core/Emails/EmailService.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Net.Mail;
+
+namespace backend.Core.Emails
+{
+    public class EmailService(IConfiguration configuration) : IEmailService
+    {
+        public Task SendEmailAsync(string to, string subject, string body, string[]? attachments = null)
+        {
+            return SendEmailsAsync([to], subject, body, attachments);
+        }
+
+        public async Task SendEmailsAsync(IEnumerable<string> to, string subject, string body, string[]? attachments = null)
+        {
+            MailMessage mailMessage = new MailMessage
+            {
+                From = new MailAddress(configuration["Email:FromAddress"], configuration["Email:FromName"]),
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true
+            };
+
+            foreach (var recipient in to)
+            {
+                mailMessage.To.Add(recipient);
+            }
+            if (attachments != null)
+            {
+                foreach (var attachment in attachments)
+                {
+                    mailMessage.Attachments.Add(new Attachment(attachment));
+                }
+            }
+            await SmtpClient.SendMailAsync(mailMessage);
+        }
+
+        public Task SendEmailsWithTemplateAsync(IEnumerable<string> to, string subject, EmailTemplate template, string body, string[]? attachments = null)
+        {
+            body = BuildWithTemplate(template, body);
+            return SendEmailsAsync(to, subject, body, attachments);
+        }
+
+        public Task SendEmailWithTemplateAsync(string to, string subject, EmailTemplate template, string body, string[]? attachments = null)
+        {
+            return SendEmailsWithTemplateAsync([to], subject, template, body, attachments);
+        }
+
+        private SmtpClient SmtpClient
+        {
+            get
+            {
+                var smtpClient = new SmtpClient(configuration["Email:SmtpServer"])
+                {
+                    Port = int.Parse(configuration["Email:SmtpPort"]),
+                    Credentials = new System.Net.NetworkCredential(configuration["Email:Username"], configuration["Email:Password"]),
+                    EnableSsl = bool.Parse(configuration["Email:EnableSsl"])
+                };
+                return smtpClient;
+            }
+        }
+
+        private static string BuildWithTemplate(EmailTemplate template, string body)
+        {
+            return template switch
+            {
+                EmailTemplate.Default => $"<h1>Example App</h1>" +
+                $"<br />{body}",
+                EmailTemplate.Auth => $"<h1>Auth</h1>" +
+                $"<br />{body}",
+                EmailTemplate.Suspicious_Behavior => $"<h1>We've noticed something</h1>" +
+                $"<br />{body}",
+                _ => $"{body}"
+            };
+        }
+    }
+}
---
+++ src/backend.Core/Emails/IEmailService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace backend.Core.Emails
+{
+    public interface IEmailService
+    {
+        public Task SendEmailAsync(string to, string subject, string body, string[]? attachments = null);
+        public Task SendEmailsAsync(IEnumerable<string> to, string subject, string body, string[]? attachments = null);
+        public Task SendEmailWithTemplateAsync(string to, string subject, EmailTemplate template, string body, string[]? attachments = null);
+        public Task SendEmailsWithTemplateAsync(IEnumerable<string> to, string subject, EmailTemplate template, string body, string[]? attachments = null);
+
+    }
+}
